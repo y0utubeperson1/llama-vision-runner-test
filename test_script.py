fb45ba54b9aa833dbbc50cd936af8d3192323a19
y0utubeperson1 <y0utubebro13@gmail.com>
2025-02-26 17:28:20 +0000

    More test images

--- test_script.py
+++ test_script.py
@@ -2,7 +2,7 @@ import requests
 import ollama
 import os, base64
 
-def load_image_file(file_path="test_image.png"):
+def load_image_file(file_path):
     with open(file_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
     return encoded_string
@@ -16,13 +16,17 @@ def output_text_from_image(img_b64):
                 'images': [img_b64]
             }]
         )
-    return get_text.message.content.replace('-', '')
+    return get_text.message.content
 
 
 def solve_answer():
-    img_resp = load_image_file()
-    text_resp = output_text_from_image(img_resp)
-    print(text_resp)
+    files = ["test_image.png", "test_image_2.png", "test_image_3.png"]
+    for file in files:
+        print("--------------------------")
+        print(f"Reading file: {file}")
+        img_resp = load_image_file(file)
+        text_resp = output_text_from_image(img_resp)
+        print(f" > {text_resp}")
 
 if __name__ == "__main__":
     print(solve_answer())
